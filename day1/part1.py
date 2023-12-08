172304101267662bdafc9bda81d8fed76ecc5abe
xinj10 <xinj10@outlook.com>
2023-12-08 11:30:47 +0800

    feat: start day1 part2

--- day1/part1.py
+++ day1/part1.py
@@ -1,14 +1,17 @@
 res = 0
 
+
 def firstDigit(s):
     for i in range(len(s)):
         if s[i].isdigit():
             return s[i]
 
+
 def lastDigit(s):
-    for i in range(1, 1+len(s)):
+    for i in range(1, 1 + len(s)):
         if s[-i].isdigit():
             return s[-i]
+
 
 with open("day1/input.txt") as f:
     lines = f.readlines()
