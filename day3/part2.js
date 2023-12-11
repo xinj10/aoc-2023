01d09443b2f1ad43630bfe35b67bbe7027c2d578
xinj10 <xinj10@outlook.com>
2023-12-11 14:54:35 +0800

    feat: day3 part 2 grid complete

--- day3/part2.js
+++ day3/part2.js
@@ -1,6 +1,40 @@
 const fs = require("fs");
 const path = require("path");
 const input = fs
-  .readFileSync(path.resolve(__dirname, "input.txt"), "utf-8")
+  .readFileSync(path.resolve(__dirname, "sample.txt"), "utf-8")
   .split("\n");
-console.log(input);
+
+const nRow = input.length;
+const nCol = input[0].length;
+
+const isDigid = (c) => /^\d$/.test(c);
+
+let pairId = 0;
+const grid = input.map((row) => {
+  const gridRow = [];
+  let accNum = "";
+  for (let i = 0; i < row.length; i++) {
+    if (isDigid(row[i])) accNum += row[i];
+    else {
+      if (accNum) {
+        for (let j = 0; j < accNum.length; j++) {
+          gridRow.push({ accNum, pairId });
+        }
+        accNum = "";
+        pairId += 1;
+      }
+      gridRow.push(row[i]);
+    }
+  }
+
+  if (accNum) {
+    for (let j = 0; j < accNum.length; j++) {
+      gridRow.push({ accNum, pairId });
+      accNum = "";
+      pairId += 1;
+    }
+  }
+  return gridRow;
+});
+
+console.log(grid);
