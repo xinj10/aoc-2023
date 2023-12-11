df3c44169e6ae99c927c9d63fd672d42d3157cdb
xinj10 <xinj10@outlook.com>
2023-12-11 15:19:47 +0800

    feat: day3 complete!

--- day3/part2.js
+++ day3/part2.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const input = fs
-  .readFileSync(path.resolve(__dirname, "sample.txt"), "utf-8")
+  .readFileSync(path.resolve(__dirname, "input.txt"), "utf-8")
   .split("\n");
 
 const nRow = input.length;
@@ -37,4 +37,37 @@ const grid = input.map((row) => {
   return gridRow;
 });
 
-console.log(grid);
+const getPairs = (r, c) => {
+  const pairs = [];
+  const pairsId = new Set();
+  for (let i = -1; i < 2; i++) {
+    const currRow = r + i;
+    if (currRow < 0 || currRow >= nRow) continue;
+    for (let j = -1; j < 2; j++) {
+      const currCol = c + j;
+      if (currCol < 0 || currCol >= nCol) continue;
+      const item = grid[currRow][currCol];
+      if (typeof item === "object" && !pairsId.has(item.pairId)) {
+        pairs.push(item);
+        pairsId.add(item.pairId);
+      }
+    }
+  }
+  return pairs;
+};
+
+let gearRatioSum = 0;
+for (let r = 0; r < nRow; r++) {
+  for (let c = 0; c < nCol; c++) {
+    if (isDigid(input[r][c]) || input[r][c] === ".") continue;
+    // gear
+    const pairs = getPairs(r, c);
+    if (pairs.length === 2) {
+      gearRatioSum +=
+        Number.parseInt(pairs[0].accNum, 10) *
+        Number.parseInt(pairs[1].accNum, 10);
+    }
+  }
+}
+
+console.log(gearRatioSum);
