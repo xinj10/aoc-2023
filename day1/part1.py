faf88d7835d30f558caae9949942bed72df54515
xinj10 <xinj10@outlook.com>
2023-12-16 10:57:11 +0800

    feat: add python solution for day1

---
+++ day1/part1.py
@@ -0,0 +1,20 @@
+import os
+import re
+import functools
+
+input_path = "input.txt"
+
+with open(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), input_path), "r"
+) as file:
+    games = file.read().splitlines()
+
+games_number = list(map(lambda x: re.findall("\d", x), games))
+
+
+def sum_num(acc, str_num_list: list[str]) -> int:
+    current = int(str_num_list[0]) * 10 + int(str_num_list[-1])
+    return acc + current
+
+
+print(functools.reduce(sum_num, games_number, 0))
